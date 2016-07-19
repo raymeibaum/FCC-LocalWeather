2bc38f639ece5633fb463d8a1d0959e1f38bc7fa
Ray Meibaum <rmeibaum@me.com>
2016-07-19 19:16:20 +0000

    local weather has been rewritten to be presented on my github.io portfolio page

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 $(function() {
   //get position
+  $("#weather").hide();
   navigator.geolocation.getCurrentPosition(function(position) {
 
     //get geocode
     $.get({
         url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '&key=AIzaSyCSCxRQ10BcXrr2_30d09T0g0xZRwr-Z1g'
       }).success(function(geoLoc) {
-        $('#city').text(geoLoc.results[3].formatted_address);
-      })
+        console.log(geoLoc);
+        $('#city').text(geoLoc.results[2].formatted_address);
+      });
 
       //get weather
     $.get({
@@ -20,35 +22,39 @@ $(function() {
       skycons.add("weatherIcon", weather.currently.icon);
       skycons.play();
 
-      var fTemp = weather.currently.temperature;
-      var cTemp = convertToCelsius(weather.currently.temperature);
+      var fTemp = Math.round(weather.currently.temperature);
+      var cTemp = Math.round(convertToCelsius(weather.currently.temperature));
+      var $fTempToggle = $('#fTempToggle');
+      var $cTempToggle = $('#cTempToggle');
+      var $temperature = $('#temperature');
 
       //update html
-      $('#fTemperature').text(Math.round(fTemp) + '\u2109');
-      $('#cTemperature').text(Math.round(cTemp) + '\u2103');
-      $('#summary').text(weather.currently.summary);
+      $temperature.text(fTemp);
+      $('#summary').append(weather.currently.summary);
       $('#wind').append(Math.round(weather.currently.windSpeed) + ' mph');
       $('#humidity').append(Math.round(weather.currently.humidity * 100) + '%');
       $('#pressure').append(Math.round(weather.currently.pressure) + ' mb')
       $('#visability').append(weather.currently.visibility + ' mi');
+      $('#weather').show();
 
-      $('#tempToggle').click(function() {
-        $('#fTemperature').toggle();
-        $('#cTemperature').toggle();
-      })
-    })
+      $fTempToggle.click(function(e) {
+        e.preventDefault();
+        $temperature.text(cTemp);
+        $fTempToggle.hide();
+        $cTempToggle.show();
+      });
+      $cTempToggle.click(function(e) {
+        e.preventDefault();
+        $temperature.text(fTemp);
+        $fTempToggle.show();
+        $cTempToggle.hide();
+      });
+    });
   }, function() {
     $('#city').text('Could not get location.');
-  })
-})
+  });
+});
 
 function convertToCelsius(temp) {
   return (temp - 32) * 5 / 9;
 }
-
-function convertCelsiusToFahrenheit(temp) {
-  return temp * 9 / 5 + 32;
-}
-
-// °F to °C	Deduct 32, then multiply by 5, then divide by 9
-// °C to °F	Multiply by 9, then divide by 5, then add 32
